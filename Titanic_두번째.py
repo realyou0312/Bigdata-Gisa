284705b0972e824fa493c68d336bffed023c7bc9
Jinyou Kim <76391662+realyou0312@users.noreply.github.com>
2021-06-12 17:11:22 +0900

    0611_Titanic_분류

---
+++ Titanic_두번째.py
@@ -0,0 +1,165 @@
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_row', 500)
+pd.set_option('display.max_columns', 100)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.metrics import roc_curve, auc, log_loss, accuracy_score
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+
+import re as re
+import warnings
+warnings.filterwarnings('ignore')
+
+SEED = 42
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+full_data = [train, test]
+
+print(train.info())
+
+# Pclass
+
+print(train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean())
+
+# Parch, SibSp
+
+for dataset in full_data:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+print(train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+
+for dataset in full_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+print(train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+# Embarked
+
+for dataset in full_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+print(train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean())
+
+# Fare
+
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+print(train[['CategoricalFare', 'Survived']].groupby(['CategoricalFare'], as_index=False).mean())
+
+
+for dataset in full_data:
+    dataset['Age'] = dataset.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
+
+def get_title(name): # Kaggle 참고
+	title_search = re.search(' ([A-Za-z]+)\.', name)
+
+	if title_search:
+		return title_search.group(1)
+	return ''
+
+for dataset in full_data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+
+for dataset in full_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+#########################################################################
+#Data Cleaning
+
+for dataset in full_data:
+
+    dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+    dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+
+drop_feats = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp', 'Parch', 'FamilySize']
+train = train.drop(drop_feats, axis=1)
+train = train.drop(['CategoricalFare'], axis=1)
+
+test = test.drop(drop_feats, axis=1)
+
+print(train.head(10))
+
+train = train.values
+test = test.values
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(probability=True),
+    RandomForestClassifier(),
+    LogisticRegression()]
+
+
+log 	 = pd.DataFrame(columns=["Classifier", "Accuracy"])
+
+X = train[0::, 1::]
+y = train[0::, 0]
+
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+
+acc_dict = {}
+
+for train_index, test_index in skfold.split(X, y):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    for clf in classifiers:
+        name = clf.__class__.__name__
+        clf.fit(X_train, y_train)
+        pred = clf.predict(X_test)
+        acc = accuracy_score(y_test, pred)
+        if name in acc_dict:
+            acc_dict[name] += acc
+        else:
+            acc_dict[name] = acc
+
+for clf in acc_dict:
+    acc_dict[clf] = acc_dict[clf] / 10.0
+    log_entry = pd.DataFrame([[clf, acc_dict[clf]]], columns=["Classifier", "Accuracy"])
+    log = log.append(log_entry)
+
+print(log)
+
+classifier = SVC()
+classifier.fit(train[0::, 1::], train[0::, 0])
+result = classifier.predict(test)
+
+# print(result)
+test_result2 = pd.Series(result, name="Survived")
+# print(test_result2)
+test = pd.read_csv('test.csv')
+results = pd.concat([test.PassengerId, test_result2],axis=1) # 왜 array?
+print(results.head(3))
+
+results.to_csv("Submission.csv", index=False)
